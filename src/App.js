67cc1690e81c362ad87a7a42d90e2662a9d89304
Chris Murphy <_chrism_@live.com>
2018-04-25 09:14:40 +0100

    removes bullets from list items

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ChartContainer from "./containers/ChartContainer";
+import './components/SongList.css';
 import './App.css';
 
 class App extends Component {
